d9a04140521daa9c2ac3dcf935ac0e785bca774d
Teboho Bodibe <tbodibe@e4.co.za>
2025-06-17 14:37:11 +0200

    Initial commit

---
+++ SAHL_Cancellations/SAHL_Cancellations/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SAHL_Cancellations
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
